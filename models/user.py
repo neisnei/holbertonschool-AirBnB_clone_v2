5f1465fa8809c8a551970128f1083e5840b0039f
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-30 14:59:50 -0400

    Add relationships for places and reviews in User model

--- models/user.py
+++ models/user.py
@@ -6,8 +6,14 @@ from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
+    """Class defines a user by various attributes"""
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship("Place", backref="user",
+                        cascade="all, delete, delete-orphan")
+    reviews = relationship("Review", backref="user",
+                        cascade="all, delete, delete-orphan")
