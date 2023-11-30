64200402d48ccd7d9ec37fb60bb1dce22c89995d
neisnei <neischalyruidiaz@gmail.com>
2023-11-30 16:04:51 +0000

    add

--- models/state.py
+++ models/state.py
@@ -3,8 +3,25 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
+from models.city import City
+from os import getenv
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete", backref="state")
+
+    else:
+        name = ''
+
+        @property
+        def cities(self):
+            """Getter"""
+            city_lst = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_lst.append(city)
+            return city_lst
