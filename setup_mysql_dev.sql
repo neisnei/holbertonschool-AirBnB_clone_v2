e193c5dbc53a90b3fd76bf3b446005fc1b0851cf
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-30 23:44:33 -0400

    Update MySQL setup scripts with user creation and privilege grants

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,14 @@
 -- prepares a MySQL server for the project
 -- GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-USE hbnb_dev_db;
+
+-- Grant privileges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-USE performance_schema;
+
+--Grant SELECT privileges
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,16 @@
 -- Prepare server
 -- Create database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
-USE hbnb_test_db;
+
+-- Create user
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant privileges
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-USE performance_schema;
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privileges
+GRANT
+SELECT
+    ON performance_schema.* TO 'hbnb_test'@'localhost'; 
+
 FLUSH PRIVILEGES;
