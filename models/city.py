e1e55b4d25337eb81e7ee30453c05858885d5e51
neisnei <neischalyruidiaz@gmail.com>
2023-12-01 18:33:18 +0000

    psycodestyle city

--- models/city.py
+++ models/city.py
@@ -13,7 +13,7 @@ class City(BaseModel, Base):
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         places = relationship("Place", backref="cities",
-                            cascade="all, delete")
+                 cascade="all, delete")
     else:
         name = ""
         state_id = ""
