8fb11e1968f3a19535828ac5986888b060d36024
neisnei <neischalyruidiaz@gmail.com>
2023-12-01 18:48:43 +0000

    psycodestyle amenity

--- models/amenity.py
+++ models/amenity.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
