033fe2385d0c1f4ec38bf1f5afcd5bb595887b42
neisnei <neischalyruidiaz@gmail.com>
2023-12-01 18:44:32 +0000

    psycodestyle amenity

--- models/amenity.py
+++ models/amenity.py
@@ -5,11 +5,10 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
-
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         place_amenities = relationship("Place", secondary="place_amenity",
-                                    back_populates="amenities")
+                                       back_populates="amenities")
