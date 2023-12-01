dbe40832c5752cd1a6dc9a0f5fd3b1455eadfe9f
neisnei <neischalyruidiaz@gmail.com>
2023-12-01 17:52:08 +0000

    psycodestyle user

--- models/user.py
+++ models/user.py
@@ -14,6 +14,6 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=True)
 
     places = relationship("Place", backref="user",
-            cascade="all, delete, delete-orphan")
+    cascade="all, delete, delete-orphan")
     reviews = relationship("Review", backref="user",
-            cascade="all, delete, delete-orphan")
+    cascade="all, delete, delete-orphan")
