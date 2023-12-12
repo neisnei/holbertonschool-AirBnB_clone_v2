5660cddff41f0439983bcba84b8a48757afb840b
neisnei <neischalyruidiaz@gmail.com>
2023-12-12 14:48:46 +0000

    start flask web

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,44 +1,54 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
+"""Starts a Flask web application."""
 from flask import Flask
 from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)  # route definition
-def hello_hbnb():  # content to display on that route
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
-# New route
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """Displays 'HBNB'"""
     return "HBNB"
 
 
-# New route
-@app.route('/c/<text>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    return "C {}" .format(text.replace("_", " "))
+    """Displays 'C' followed by the value of <text>
+
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-# New route
-@app.route('/python/<text>', strict_slashes=False)
-@app.route('/python', strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
-    return "Python {}" .format(text.replace("_", " "))
+    """Displays 'Python' followed by the value of <text>
+
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
-# New route
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    return "{} is a number" .format(n)
+    """Displays 'n is a number' only if <n> is an integer."""
+    return "{} is a number".format(n)
 
 
-# New route
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', n=n)
+    """Displays an HTML page only if <n> is an integer."""
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == '__main__':
