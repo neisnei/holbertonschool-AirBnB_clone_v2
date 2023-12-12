6f914f0a999b5309d076835553f91e05a44ff42f
nataliagrivera <nataliagirvera62@gmail.com>
2023-12-11 23:13:29 -0400

    Add new routes to Flask web application

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Start a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)  # route definition
+def hello_hbnb():  # content to display on that route
+    return "Hello HBNB!"
+
+
+# New route
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+# New route
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C {}" .format(text.replace("_", " "))
+
+
+# New route
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    return "Python {}" .format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
