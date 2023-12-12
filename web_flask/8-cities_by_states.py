be620890a6a448058c35f31c87e1a7b69f6dd979
neisnei <neischalyruidiaz@gmail.com>
2023-12-12 16:01:15 +0000

    state py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+script starts Flask web application
+'''
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    state_li = storage.all(State).values()
+    return render_template('7-states_list.html', states=state_li)
+
+
+@app.route('cities_by_states', strict_slashes=False)
+def cities_by_states():
+    city_li = storage.all(State).values()
+    return render_template('8-cities_by_states.html', cities=city_li)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
