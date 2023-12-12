3288af129c38bd3b89d7d8a45f6690d822e275a3
neisnei <neischalyruidiaz@gmail.com>
2023-12-12 16:13:02 +0000

    state py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,24 +10,22 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states_route():
-    """
-    Cities by states: display a HTML page: (inside the tag BODY)
-    Returns:
-        html: template that lists all states sort by name A->Z
-    """
-    states = storage.all("State").values()
-    return render_template("8-cities_by_states.html", states=states)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    state_li = storage.all(State).values()
+    return render_template('7-states_list.html', states=state_li)
+
+
+@app.route('cities_by_states', strict_slashes=False)
+def cities_by_states():
+    city_li = storage.all(State).values()
+    return render_template('8-cities_by_states.html', cities=city_li)
 
 
 @app.teardown_appcontext
-def close_db(exception=None):
-    """
-    After each request remove the current SQLAlchemy Session:
-    """
+def teardown_appcontext(exception):
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
