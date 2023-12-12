41b8dbe19da362bdc085ed4f2c80b46641e64be4
neisnei <neischalyruidiaz@gmail.com>
2023-12-12 16:06:09 +0000

    state py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-'''
+
+"""
 Module script that starts a Flask web application
-'''
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
