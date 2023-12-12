d348e5b151236b0672c42f7f1f9c63b637c7a8e9
nataliagrivera <nataliagirvera62@gmail.com>
2023-12-12 09:58:07 -0400

    Add Flask route for number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Start a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)  # route definition
+def hello_hbnb():  # content to display on that route
+    return "Hello HBNB!"
+
+
+# New route
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+# New route
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C {}" .format(text.replace("_", " "))
+
+
+# New route
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python(text="is cool"):
+    return "Python {}" .format(text.replace("_", " "))
+
+
+# New route
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return "{} is a number" .format(n)
+
+
+# New route
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
