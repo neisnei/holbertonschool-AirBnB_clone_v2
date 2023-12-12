90ab71f2988a351a7e6d050edb7d47574d84f79c
nataliagrivera <nataliagirvera62@gmail.com>
2023-12-11 23:24:06 -0400

    Add new route for Python endpoint

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,6 +23,7 @@ def c(text):
 
 # New route
 @app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 def python(text="is cool"):
     return "Python {}" .format(text.replace("_", " "))
 
