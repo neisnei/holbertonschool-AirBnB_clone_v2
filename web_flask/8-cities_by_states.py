623d2261a2d2db5feee28c3a700dc5fbc8217b07
neisnei <neischalyruidiaz@gmail.com>
2023-12-14 21:40:59 +0000

    up up

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,57 +1,58 @@
 #!/usr/bin/python3
-""" This module starts a Flask web application """""
-from flask import Flask, render_template
+"""This module contain a Flask instance"""
+from flask import Flask, escape, render_template
 from models import storage
 from models.state import State
 from models.city import City
 
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def display_text():
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello():
+    """Function that starts a Flask web application"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
-def display_hbnb():
-    return "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hello_2():
+    """Function that starts a Flask web application"""
+    return 'HBNB'
 
 
-@app.route("/c/<text>")
-def c_text(text):
-    return "C %s" % text.replace('_', ' ')
+@app.route('/c/<text>', strict_slashes=False)
+def hello_3(text):
+    """Function that starts a Flask web application"""
+    return 'C ' + f'{escape(text)}'.replace('_', ' ')
 
 
-@app.route("/python/", strict_slashes=False, defaults={'text': 'is cool'})
-@app.route("/python/<text>", strict_slashes=False)
-def python_text(text):
-    if text == "is cool":
-        return "Python is cool"
-    else:
-        return "Python " + f'{text}'.replace("_", " ")
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def hello_4(text):
+    """Function that starts a Flask web application"""
+    return 'Python ' + f'{escape(text)}'.replace('_', ' ')
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def n_text(n):
+@app.route('/number/<int:n>', strict_slashes=False)
+def hello_5(n):
+    """Function that starts a Flask web application"""
     if type(n) == int:
-        return f'{n} is a number'
+        return f'{escape(n)}' + ' is a number'
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n):
-    return render_template('5-number.html', number=n)
+@app.route('/number_template/<int:n>')
+def hello_6(n=None):
+    return render_template('5-number.html', n=n)
 
 
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def number_template1(n=None):
-    a = ""
-    if n % 2 == 0:
-        a = "even"
+@app.route('/number_odd_or_even/<int:n>')
+def hello_7(n=None):
+    result = ""
+    if (n % 2) == 0:
+        result = "even"
     else:
-        a = "odd"
-    return render_template('6-number_odd_or_even.html', number=n, a=a)
+        result = "odd"
+    return render_template('6-number_odd_or_even.html', n=n, result=result)
 
 
 @app.route('/states_list', strict_slashes=False)
@@ -60,23 +61,17 @@ def hello_8():
     return render_template('7-states_list.html', states=states_dict.values())
 
 
-@app.teardown_appcontext
-def s_close(exception):
-    storage.close()
-
-
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
-
-    return render_template('8-cities_by_states.html', states=states)
+def hello_9():
+    states_dict = storage.all(State)
+    return (render_template('8-cities_by_states.html',
+                            states=states_dict.values()))
 
 
 @app.teardown_appcontext
-def close_storage(exception):
+def close_sql(exception):
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
