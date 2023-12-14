51a135596edeed2be0248b04523e84cbc21f787e
neisnei <neischalyruidiaz@gmail.com>
2023-12-14 21:17:30 +0000

    up

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web app
-"""
-
-
-from flask import Flask, render_template
+""" This module starts a Flask web application """""
+from flask import Flask
+from flask import render_template
 from models import storage
-
+from models.state import State
+""" Flask class and render_template method"""""
 app = Flask(__name__)
-
-
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    states = storage.all(State)
-    return render_template(
-        "cities_by_states.html", states=states, strict_slashes=False
-    )
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def close_session(exception=None):
+def teardown(self):
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/cities_by_states')
+def cities_by_states():
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
