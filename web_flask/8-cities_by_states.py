25c368038941c8dbaa2dd9829b3002dd22a96751
neisnei <neischalyruidiaz@gmail.com>
2023-12-15 15:48:33 +0000

    fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3
-""" This module starts a Flask web application """""
-from flask import Flask
-from flask import render_template
+"""
+Script that starts a Flask web application.
+"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-""" Flask class and render_template method"""""
+from models.city import City
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Route taht displays the html page of
+    a list of states objects sorted by name
+    """
+    states_li = storage.all(State).values()
+    return render_template('7-states_list.html', states=states_li)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ Route that display a HTML page with a list of cities
+    objects sorted by name """
+    city_li = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=city_li)
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown_appcontext(exception):
+    """After each request, remove the SQLAlchemy Session"""
     storage.close()
 
 
-@app.route('/cities_by_states')
-def cities_by_states():
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
