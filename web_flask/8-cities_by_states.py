bf1dd3132295e23a7a57c63adac3ccea794faa52
neisnei <neischalyruidiaz@gmail.com>
2023-12-15 19:32:09 +0000

    fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Display a HTML page with a list of all State objects and their cities."""
+    """Display a HTML page with a list of all
+    State objects and their cities."""
     states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
