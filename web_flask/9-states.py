16e34b6c0981d05b810a0ea163d116d9270e1b1e
neisnei <neischalyruidiaz@gmail.com>
2023-12-15 16:54:43 +0000

    fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,46 +1,88 @@
 #!/usr/bin/python3
-""" This script that starts a Flask web application """
-
-from flask import Flask, render_template
+"""This is the module documentation"""
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
-
-
+from models.city import City
+"""
+Script that starts a Flask
+Web app for my HBNB clone project
+"""
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ Route that display a HTML page with a list of states
-    objects sorted by name """
-    state_li = storage.all(State).values()
-    return render_template('7-states_list.html', states=state_li)
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """ Route that display a HTML page with a list of cities
-    objects sorted by name """
-    city_li = storage.all(State).values()
-    return render_template('8-cities_by_states.html', cities=city_li)
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    return "C " + f'{text}'.replace("_", " ")
+
+
+@app.route("/python/", strict_slashes=False, defaults={'text': 'is cool'})
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    if text == "is cool":
+        return "Python is cool"
+    else:
+        return "Python " + f'{text}'.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return f'{n} is a number'
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    all_states = storage.all(State).values()
+    return render_template('7-states_list.html',
+                           all_states=all_states, header="States")
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_state():
+    all_states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', all_states=all_states,
+                           header="States")
+
+
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
 def states(id=None):
-    state_dic = storage.all(State)
-    state = None
-    for obj in state_dic.values():
-        if obj.id == id:
-            state = obj
-            return render_template('9-states.html', states=state_dic, id=id,
-                        state=state)
+    if id:
+        try:
+            state = storage.all(State)[f'State.{id}']
+            return render_template('9-states.html', state=state,
+                                   header="States", id=id)
+        except KeyError:
+            return render_template('9-states.html', error="Error",)
+    else:
+        all_states = storage.all(State).values()
+        return render_template('9-states.html',
+                               all_states=all_states, header="States")
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
-    """ Function that removes the current SQL Alchemy Session after each
-    request. """
     storage.close()
 
 
