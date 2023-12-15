161beb77282c882624f76924a6184d303e7f59c7
neisnei <neischalyruidiaz@gmail.com>
2023-12-15 17:25:36 +0000

    fix update

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,36 +1,23 @@
 #!/usr/bin/python3
-"""
-A script that starts a Flask web application:
-"""
-
-from flask import Flask
+"""Write a script that starts a Flask web application"""
+from flask import Flask, render_template
 from models import storage
-from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def states_list_route():
-    """
-    List all cities a of states: display a HTML page: (inside the tag BODY)
-    Returns:
-        html: template that lists states, cities & amenity sort by name A->Z
-    """
-    data = {
-        "states": storage.all("State").values(),
-        "amenities": storage.all("Amenity").values()
-    }
-    return render_template("10-hbnb_filters.html", models=data)
+@app.route('/hbnb_filters', strict_slashes = False)
+def hbnb_filters():
+    states = sorted(storage.all("State").values(), key=lambda x: x.name)
+    cities = sorted(storage.all("City").values(), key=lambda x: x.name)
+    amenities = sorted(storage.all("Amenity").values(), key=lambda x: x.name)
+    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
 
 
 @app.teardown_appcontext
-def close_db(exception=None):
-    """
-    After each request remove the current SQLAlchemy Session:
-    """
+def close_storage(exception):
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
