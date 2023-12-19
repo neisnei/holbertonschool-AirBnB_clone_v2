d24249f9bac6e4fc5dcae2624012288bcc963af6
neisnei <neischalyruidiaz@gmail.com>
2023-12-19 14:28:55 +0000

    add code for fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
-"""
-Script that starts a Flask web application.
-"""
+"""Start a Flask web application"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
-
+from models.city import City
 app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page with a list of states"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Display a HTML page with a list of all
-    State objects and their cities."""
+    """Display a HTML page with a list of states"""
     states = storage.all(State).values()
-    return render_template('8-cities_by_states.html', states=states)
+    cities = storage.all(City).values()
+    return render_template('8-cities_by_states.html', states=states,
+                           cities=cities)
 
 
 @app.teardown_appcontext
-def close_storage(exc):
-    """Remove the current SQLAlchemy Session."""
+def teardown_db(exception):
+    """Close storage"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
