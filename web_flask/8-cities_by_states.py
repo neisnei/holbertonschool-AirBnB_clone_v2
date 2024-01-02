674f13bb30ba413c0df97c74b15d0861d3c66827
neisnei <neischalyruidiaz@gmail.com>
2024-01-02 13:37:15 +0000

    fix file

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -21,10 +21,11 @@ def states_list():
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """ Route that display a HTML page with a list of cities
+    """ Route that displays a HTML page with a list of cities
     objects sorted by name """
-    city_li = storage.all(State).values()
-    return render_template('8-cities_by_states.html', states=city_li)
+    states = storage.all(State).values()
+    cities = storage.all(City).values()
+    return render_template('8-cities_by_states.html', states=states, cities=cities)
 
 
 @app.teardown_appcontext
